cf420603dcffa94aa721186fa56ce6f1b9539000
Antonio <antonio.rodriguez024@gmail.com>
2021-12-03 00:28:39 -0600

    added navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import {Link} from 'react-scroll';
+import logo from '../images/logo1.png';
 
 function NavBar() {
-    return (
-        <div>
 
-        </div>
+    const[nav, setnav] = useState(false);
+
+    const changeBackGround = () => {
+        if(window.scrollY >= 50) {
+            setnav(true);
+        }
+        else {
+            setnav(false);
+        }
+    }
+
+    window.addEventListener('scroll',changeBackGround);
+
+    return (
+        <nav className={nav ? "nav active" :"nav"}>
+            <Link to='main' className='logo' >
+                <img src={logo} alt='' />
+            </Link>
+            <input className='menu-btn' type='checkbox' id='menu-btn' />
+            <label className='menu-icon' for='menu-btn'>
+                <span className="nav-icon"></span> 
+            </label>
+            <ul className='menu'>
+                <li><Link to="#">Header</Link></li>
+                <li><Link to="#">Products</Link></li>
+                <li><Link to="#">About</Link></li>
+                <li><Link to="#">Contact</Link></li>
+            </ul>
+        </nav>
     )
 }
 
