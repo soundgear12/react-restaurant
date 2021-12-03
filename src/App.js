0e7f8523c3a17621e72f5d50eb0a889f5d5fb778
Antonio <antonio.rodriguez024@gmail.com>
2021-12-03 00:17:23 -0600

    added PropagatedLoader

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React, { useState, useEffect} from 'react';
 import './App.css';
 import NavBar from './Components/Navbar';
 import {css} from "@emotion/react";
+import PropagateLoader from 'react-spinners/PropagateLoader';
+import reactDom from 'react-dom';
 
 function App() {
+
+  const[loading, setLoading] = useState(false);
+  const override = css`
+  display:block;
+  border-color:red;
+  margin-top:20%;
+  `;
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000);
+  },[])
   return (
     <div className="App">
       
+      {
+        loading ? <PropagateLoader color={"#3d2514"} loading={loading} css={override} size={40}/>
+        :
+        <>
+        <NavBar />
+        </>  
+      }
+
     </div>
   );
 }
